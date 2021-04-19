25860898c1ea91b6f4564aab372ef478418d6399
Alban Fichet <alban.fichet@gmx.fr>
2021-04-19 17:38:18 +0200

    Make the plugin working with CMake

--- src/main.c
+++ src/main.c
@@ -19,7 +19,7 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <config.h>
+// #include <config.h>
 #include <gtk/gtk.h>
 #include <gthumb/gthumb.h>
 #include <glib.h>
